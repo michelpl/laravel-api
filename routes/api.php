f613161276ce318181f2a02102b6aceb4069576c
Michel <michelpl@yahoo.com.br>
2018-03-16 11:28:43 -0300

    :sparkles: Add new routes.

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,55 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/', function () {
+    return 'Laravel API';
+});
+
+
+Route::prefix('/products')->group(function () {
+
+    Route::get('/', function () {
+        return [
+            15 => [
+                'name' => 'Iphone',
+                'description' => "iPhone is a revolutionary new mobile phone that allows you to make a call by simply tapping a nam.."
+            ],
+            17 => [
+                'name' => 'Canon 110',
+                'description' => "Canon's press material for the EOS 5D states that it defines (a) new D-SLR category"
+            ],
+            35 => [
+                'name' => 'Nikon D300',
+                'description' => "Engineered with pro-level features and performance, the 12.3-effective-megapixel D300 combines brand"
+            ]
+        ];
+    });
+
+    Route::get('{id}', function ($id) {
+        $products = [
+            15 => [
+                'name' => 'Iphone',
+                'description' => "iPhone is a revolutionary new mobile phone that allows you to make a call by simply tapping a nam.."
+            ],
+            17 => [
+                'name' => 'Canon 110',
+                'description' => "Canon's press material for the EOS 5D states that it defines (a) new D-SLR category"
+            ],
+            35 => [
+                'name' => 'Nikon D300',
+                'description' => "Engineered with pro-level features and performance, the 12.3-effective-megapixel D300 combines brand"
+            ]
+        ];
+
+        return $products[$id] ?? response('', 404);
+    });
+});
+
+/*Route::get('/', function (Request $request){
+    return ['A', 'B'];
+});
+
+Route::post('/', function (Request $request){
+    return ['A', 'B'];
+});*/
